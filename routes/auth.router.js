67000a16c07a335a454b0afeda08af806a539de6
Abhishek <137991776+iamstufff@users.noreply.github.com>
2023-12-11 20:17:30 +0530

    Add files via upload

---
+++ routes/auth.router.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const singupHandler = require("../controllers/signupController");
+const loginHandler = require("../controllers/loginController");
+
+const router = express.Router();
+
+router.route("/register")
+    .post(singupHandler)
+
+router.route("/login")
+    .post(loginHandler)
+
+module.exports = router;
+// const router = require('express').Router()
+
+// router.post('/register', singupHandler.singupHandler);
